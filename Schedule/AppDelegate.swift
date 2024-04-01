380d93327440ad2bc05a49b6b8c259adde8bc734
Ilya Shevchenko <95874401+InkaShev7777@users.noreply.github.com>
2024-04-01 14:11:31 +0300

    delete storyboard

---
+++ Schedule/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Schedule
+//
+//  Created by Ilya Schevchenko on 01.04.2024.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+}
+
---
+++ Schedule/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Schedule
+//
+//  Created by Ilya Schevchenko on 01.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
